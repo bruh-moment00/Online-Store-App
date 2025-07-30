cf29422648440494d300efb4c86ed98df662931e
Nikita Belokon <nikitabelokon@mail.ru>
2025-07-30 10:42:33 +0300

    DB: updated scripts for creating database

--- DB/First employee script.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.employee(
-	"FirstName", "LastName", "PhoneNum", "Email", "BirthDate", "Login", "PasswordHash", "CreatedDateTime", "IsActive", "IsDeleted")
-	VALUES ('Первый', 'Сотрудник', '88005553535', 'admin@onlinestore.com', '2000-06-14', 'admin', '$2a$11$zbiqinEwNtBBJGxmeLGrUeXOUJ1Q53Gr4PGeJJ/148d9Wg3AWxJoW', '2025-06-14', true, false);
---
+++ DB/create_admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.employee(
+	"FirstName", "LastName", "PhoneNum", "Email", "BirthDate", "Login", "PasswordHash", "CreatedDateTime", "IsActive", "IsDeleted")
+	VALUES ('Первый', 'Сотрудник', '88005553535', 'admin@onlinestore.com', '2000-06-14', 'admin', '$2a$11$dh.Ven4JNwTjl8p.FyobGO8Wwnq8bA1hhfNC6eUnLoHJOeSDeBRGG', '2025-06-14', true, false);
