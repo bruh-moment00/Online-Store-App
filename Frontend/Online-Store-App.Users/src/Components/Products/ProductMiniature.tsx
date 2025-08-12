9a8ec915c377d64c69ebf4fa73a803c033d5b9de
Nikita Belokon <nikitabelokon@mail.ru>
2025-08-12 16:59:39 +0300

    Frontend: can add product to cart

--- Frontend/Online-Store-App.Users/src/Components/Products/ProductMiniature.tsx
+++ Frontend/Online-Store-App.Users/src/Components/Products/ProductMiniature.tsx
@@ -4,6 +4,7 @@ import { Button, Card } from "react-bootstrap";
 import type { Product } from "commonlib/src/Models/Data/Product";
 import { getImagesURLByProductId } from "commonlib/src/Services/DataOperations/ProductImagesService";
 import { server } from "commonlib/src/AppSettings";
+import { getCookie, setCookie } from "typescript-cookie";
 
 interface Props {
     data: Product;
@@ -11,7 +12,8 @@ interface Props {
 
 export const ProductMiniature = ({ data }: Props) => {
     const [imagePreview, setImagePreview] = React.useState<string | undefined>(undefined);
-
+    const [inCart, setInCart] = React.useState<boolean>(false);
+    
     React.useEffect(() => {
         const doGetImage = async (productId: number) => {
             const foundImages = await getImagesURLByProductId(productId);
@@ -24,7 +26,26 @@ export const ProductMiniature = ({ data }: Props) => {
         if (data) {
             doGetImage(data.id);
         }
-    }, [data])
+    }, [data]);
+
+    React.useEffect(() => {
+        const getCart = async () => {
+            const cart = await getCookie("cart");
+            cart?.split(';').forEach(element => {
+                if (Number(element) === data.id)
+                    setInCart(true);
+            });
+        }
+        if (data) {
+            getCart();
+        }
+    }, [])
+
+    const handleCartClick = () => {
+        const cart = getCookie("cart");
+        setCookie("cart", cart + `;${data.id}`);
+        setInCart(true);
+    }
 
     return (
         <div className="col-xs-12 col-sm-12 col-md-4">
@@ -34,7 +55,11 @@ export const ProductMiniature = ({ data }: Props) => {
                 <Card.Body>
                     <Card.Title>{data.name}</Card.Title>
                     <Card.Text>{data.description}</Card.Text>
-                    <Button variant="outline-primary">В корзину</Button>
+                    <Card.Text>Цена {data.price} у.е.</Card.Text>
+                    {inCart ? 
+                    <Button variant="primary" onClick={handleCartClick}>В корзине</Button> :
+                    <Button variant="outline-primary" onClick={handleCartClick}>В корзину</Button>
+                    }
                 </Card.Body>
             </Card>
         </div>
